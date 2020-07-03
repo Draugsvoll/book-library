a40c603d2f40fe25abaab5b5ea76d33947f42363
Draugsvoll <ove.hd91@gmail.com>
2020-07-03 14:54:18 +0200

    små-fiks

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection 
 db.on('error', error => console.error(error))    // sjekk for error på oppkobling
-db.once('open', () => console.log('Connected to Moongoose'))     // only once, on 'open' db 
+db.once('open', () => {console.log('Connected to Moongoose')})     // show message once on open
 
 // må sette opp body-parser BEFORE routers (to make it work)
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))  // sending values via URL to server (access params)
 
 // henter routes
-const indexRouter = require('./routes/index')       // henter indexRoute fra index.js (via modules.exports)
-const authorRouter = require('./routes/authors')    // husk å /use route nedenfor
+const indexRouter = require('./routes/index')       // henter indexRoute fra index route (via modules.exports)
+const authorRouter = require('./routes/authors')   
 const bookRouter = require('./routes/books')
 
 // setup views
